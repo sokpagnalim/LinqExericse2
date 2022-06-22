a06b58b56bee853d7792ee76839d5a439847bf09
Sokpagna Lim <sokpagnalim@gmail.com>
2022-06-22 01:01:03 -0700

    initial commit exercise 2

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            List<string> videoGames = new List<string>() { "Minecraft", 
+                "Grand Theft Auto 5", 
+                "Animal Crossing: New Horizon", 
+                "Call Of Duty Warzone" };
+            var orderVideoNameLength = videoGames.OrderBy(i => i.Length).ToList();
+
+            foreach (var game in orderVideoNameLength)
+            {
+                Console.WriteLine(game);
+            }
+            //orderVideoNameLength.ForEach(Console.WriteLine);
+        }
+    }
+}
